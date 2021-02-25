ac6ab88d28b8a8890452b48210c649b373b21c14
vermad843 <vishnuverma7524@gmail.com>
2021-02-26 03:54:17 +0530

    modal

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [x] npm i firebase
+
+[x] imageUrl - https://images.unsplash.com/photo-1515342724109-4653eccad95f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=2000&fit=max&ixid=eyJhcHBfaWQiOjExNzczfQ
--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import React, {useState,useEffect} from 'react';
 import './App.css';
 import Post from './Post';
 import {db} from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import {Button} from '@material-ui/core';
+
+
+
+function getModalStyle() {
+  const top = 50; 
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -12,10 +43,18 @@ function App() {
     })
   }, []);
 
-  console.log(posts);
+
 
   return (
     <div className="app">
+       <Modal
+           open={open}
+           onClose={() => setOpen(false)}
+          >
+          <div style={modalStyle} className={classes.paper}>
+              <h2>I am modal</h2>
+         </div>
+       </Modal>
        <div className = "app__header">
           <img
             className = "app__headerImage"
@@ -23,6 +62,9 @@ function App() {
             alt = ""
           />
        </div>
+       <Button onClick={() => setOpen(true)}>
+          Sign Up
+       </Button>
         {
           posts.map((post, id) => 
              (<Post
