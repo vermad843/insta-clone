48c0b3b7a77f935367c2a872879df1e26bf0f0d8
vermad843 <vishnuverma7524@gmail.com>
2021-02-26 04:25:01 +0530

    form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Post from './Post';
 import {db} from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import {Button} from '@material-ui/core';
+import {Button, Input} from '@material-ui/core';
 
 
 
@@ -36,6 +36,9 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -43,7 +46,9 @@ function App() {
     })
   }, []);
 
+  const signUp = (event) => {
 
+  }
 
   return (
     <div className="app">
@@ -52,15 +57,42 @@ function App() {
            onClose={() => setOpen(false)}
           >
           <div style={modalStyle} className={classes.paper}>
-              <h2>I am modal</h2>
+            <form className = "app__signup">
+                <center>
+                  <img
+                      className = "app__headerImage"
+                      src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                      alt = ""
+                    />
+                </center>
+                    <Input
+                       placeholder = "username"
+                       type = "text"
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input
+                       placeholder = "email"
+                       type = "text"
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}
+                    />
+                     <Input
+                       placeholder = "password"
+                       type = "password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button onClick= {signUp} >Sign Up</Button>
+            </form>         
          </div>
        </Modal>
        <div className = "app__header">
-          <img
-            className = "app__headerImage"
-            src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-            alt = ""
-          />
+            <img
+                className = "app__headerImage"
+                src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt = ""
+                     />
        </div>
        <Button onClick={() => setOpen(true)}>
           Sign Up
